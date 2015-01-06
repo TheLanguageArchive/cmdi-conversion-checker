1f3c19ffbdccd02a3754e62ecf2da941242b4517
Twan Goosen <twan.goosen@mpi.nl>
2015-01-06 15:46:33 +0100

    git init

---
+++ cmdi2imdi.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+OUTPUT_DIR=imdi-out
+SAXON_JAR=/Users/twan/.m2/repository/net/sf/saxon/Saxon-HE/9.5.1-8/Saxon-HE-9.5.1-8.jar
+STYLESHEET=/Users/twan/git/MetadataTranslator/Translator/src/main/resources/templates/cmdi2imdi/cmdi2imdiMaster.xslt
+
+if [ -d "$OUTPUT_DIR" ]
+then
+	echo Output directory \"${OUTPUT_DIR}\" already exists
+else
+	for IN_FILE in $@
+	do
+		DIRNAME=$(dirname $IN_FILE)
+		FILENAME=$(basename $IN_FILE)
+
+		TARGET_DIR=${OUTPUT_DIR}/${DIRNAME}
+		mkdir -p $TARGET_DIR
+
+		TARGET=${TARGET_DIR}/${FILENAME}.imdi
+
+		echo $FILENAME "->" $TARGET
+		java -cp ${SAXON_JAR} net.sf.saxon.Transform ${IN_FILE} ${STYLESHEET} > ${TARGET}
+	done
+fi
