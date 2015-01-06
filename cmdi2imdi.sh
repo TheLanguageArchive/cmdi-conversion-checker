cdad8673108a11435fd49dd32e52297482fdd64b
Twan Goosen <twan.goosen@mpi.nl>
2015-01-06 16:17:57 +0100

    Script documentation Renamed original-imdi directory to imdi-original

--- cmdi2imdi.sh
+++ cmdi2imdi.sh
@@ -1,6 +1,26 @@
 #!/bin/sh
+#
+# Author: twan.goosen@mpi.nl
+# Date: 6 January 2015
+#
+# Script that batch converts CMDIs to IMDI using the cmdi2imdi stylesheet from the
+# metadata translator
+#
+# Add the set of files to be converted as parameters to this script. The directory
+# structure will be matched in the output directory. All "{NAME}.cmdi" files will be
+# renamed to "{NAME}.imdi".
+#
+# There are no command line options, any configuration should be done in this file.
+#
+# 
+
+# Where to output the IMDIs
 OUTPUT_DIR=imdi-out
+
+# Location of Saxon-HE 9 JAR
 SAXON_JAR=/Users/twan/.m2/repository/net/sf/saxon/Saxon-HE/9.5.1-8/Saxon-HE-9.5.1-8.jar
+
+# CMDI-IMDI stylesheet location
 STYLESHEET=/Users/twan/git/MetadataTranslator/Translator/src/main/resources/templates/cmdi2imdi/cmdi2imdiMaster.xslt
 
 if [ -d "$OUTPUT_DIR" ]
