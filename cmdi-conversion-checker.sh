db4caaac1f4c98321569440570b6132aff0b7149
Twan Goosen <twan.goosen@mpi.nl>
2015-01-12 13:54:04 +0100

    Documentation of the script

--- cmdi-conversion-checker.sh
+++ cmdi-conversion-checker.sh
@@ -1,1 +1,53 @@
-#!/bin/sh
+#!/bin/bash
+#
+# Author: twan.goosen@mpi.nl
+# Date: 15 January 2015
+#
+# Script that takes a hierarchy of IMDI records and a parallel hierarchy of converted CMDI
+# records and validates the conversion by converting the CMDIs back to IMDI (using the
+# stylesheets from the MetadataTranslator and comparing the originals with the conversion
+# output by means of the ImdiDiff tool.
+#
+# Metadata Translator: <https://github.com/TheLanguageArchive/MetadataTranslator>
+# IMDI Diff: <https://github.com/TheLanguageArchive/ImdiDiff>
+#
+# Parameters:
+# 	1. Location of directory with original IMDIs
+#	2. Location of CMDIs derived from this IMDI (same directory structure assumed)
+
+# Set this to the jar-with-dependencies of the ImdiDiffer
+# (obtain and build from <https://github.com/TheLanguageArchive/ImdiDiff> if needed)
+IMDI_DIFF_JAR=~/git/ImdiDiff/target/ImdiDiff-1.0-SNAPSHOT-jar-with-dependencies.jar
+
+DIR=$(dirname ${BASH_SOURCE})
+CMDI2IMDI=${DIR}/cmdi2imdi.sh
+IMDI_OUTPUT_DIR=$DIR/imdi-out
+
+ORIGINAL_IMDI_DIR=$1
+CONVERTED_CMDI_DIR=$2
+
+# ok to remove old output directory? 
+if [ -d "$IMDI_OUTPUT_DIR" ]
+then
+	read -p "\"${IMDI_OUTPUT_DIR}\" already exists, ok to remove? (y/n)" -n 1 -r
+	echo ""
+	if [[ $REPLY =~ ^[Yy]$ ]]
+	then
+		echo "Removing $IMDI_OUTPUT_DIR..."
+    	rm -rf $IMDI_OUTPUT_DIR
+    else 
+    	exit 1
+	fi
+fi
+
+# convert cmdi to imdi
+echo ---------------------------------
+echo Converting CMDI output to IMDI...
+echo ---------------------------------
+bash ${CMDI2IMDI} ${CONVERTED_CMDI_DIR}
+
+# run checker
+echo ------------------
+echo Performing diff...
+echo ------------------
+java -jar ${IMDI_DIFF_JAR} ${ORIGINAL_IMDI_DIR} ${IMDI_OUTPUT_DIR}/${CONVERTED_CMDI_DIR}
--- cmdi2imdi.sh
+++ cmdi2imdi.sh
@@ -1,12 +1,12 @@
-#!/bin/sh
+#!/bin/bash
 #
 # Author: twan.goosen@mpi.nl
-# Date: 6 January 2015
+# Date: 15 January 2015
 #
 # Script that batch converts CMDIs to IMDI using the cmdi2imdi stylesheet from the
 # metadata translator
 #
-# Add the set of files to be converted as parameters to this script. The directory
+# Add the directories to be converted as parameters to this script. The directory
 # structure will be matched in the output directory. All "{NAME}.cmdi" files will be
 # renamed to "{NAME}.imdi".
 #
@@ -14,31 +14,43 @@
 #
 # 
 
-# Where to output the IMDIs
-OUTPUT_DIR=imdi-out
-
 # Location of Saxon-HE 9 JAR
-SAXON_JAR=/Users/twan/.m2/repository/net/sf/saxon/Saxon-HE/9.5.1-8/Saxon-HE-9.5.1-8.jar
+export SAXON_JAR=~/.m2/repository/net/sf/saxon/Saxon-HE/9.5.1-8/Saxon-HE-9.5.1-8.jar
+
+# Location of cmdi2imdi stylesheet
+export STYLESHEET_URL=https://github.com/TheLanguageArchive/MetadataTranslator.git
+
+# Where to output the IMDIs
+export OUTPUT_DIR=imdi-out
 
 # CMDI-IMDI stylesheet location
-STYLESHEET=/Users/twan/git/MetadataTranslator/Translator/src/main/resources/templates/cmdi2imdi/cmdi2imdiMaster.xslt
+export STYLESHEET_DIR=/tmp/cmdi2imdi-`date +"%s"`
+export STYLESHEET=${STYLESHEET_DIR}/Translator/src/main/resources/templates/cmdi2imdi/cmdi2imdiMaster.xslt
 
+function convert_to_imdi {
+	IN_FILE=$1
+	DIRNAME=$(dirname $IN_FILE)
+	FILENAME=$(basename $IN_FILE)
+
+	TARGET_DIR=${OUTPUT_DIR}/${DIRNAME}
+	mkdir -p $TARGET_DIR
+
+	TARGET_FILE=`echo ${FILENAME}|sed -e 's/\.cmdi$/.imdi/g'`
+	TARGET=${TARGET_DIR}/${TARGET_FILE}
+
+	echo $FILENAME "->" $TARGET
+	java -cp ${SAXON_JAR} net.sf.saxon.Transform ${IN_FILE} ${STYLESHEET} > ${TARGET}
+}
+export -f convert_to_imdi
+
+# Get latest stylesheet
+echo Retrieving CMDI2IMDI stylesheet...
+git clone ${STYLESHEET_URL} ${STYLESHEET_DIR}
+
+# Run conversion
 if [ -d "$OUTPUT_DIR" ]
 then
 	echo Output directory \"${OUTPUT_DIR}\" already exists
 else
-	for IN_FILE in $@
-	do
-		DIRNAME=$(dirname $IN_FILE)
-		FILENAME=$(basename $IN_FILE)
-
-		TARGET_DIR=${OUTPUT_DIR}/${DIRNAME}
-		mkdir -p $TARGET_DIR
-
-		TARGET_FILE=`echo ${FILENAME}|sed -e 's/\.cmdi$/.imdi/g'`
-		TARGET=${TARGET_DIR}/${TARGET_FILE}
-
-		echo $FILENAME "->" $TARGET
-		java -cp ${SAXON_JAR} net.sf.saxon.Transform ${IN_FILE} ${STYLESHEET} > ${TARGET}
-	done
+	find $@ -name "*.cmdi" -exec bash -c "convert_to_imdi {}" \;
 fi
